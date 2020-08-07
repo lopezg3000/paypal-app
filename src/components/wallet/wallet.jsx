591764e2e6c3af86d585f0036f9fc08f1dd4fade
Gabriel Lopez <lopezg3000@gmail.com>
2020-08-06 20:56:10 -0500

    padding for bottom of no balance section added as well as button styled for max width 768px

--- src/components/wallet/wallet.jsx
+++ src/components/wallet/wallet.jsx
@@ -3,7 +3,7 @@ import NoBalance from './noBalance';
 
 const Wallet = () => {
     return (
-        <section className='no-balance section' style={{ background: '#f5f7fa', height: '100vh' }}>
+        <section className='no-balance section' style={{ background: '#f5f7fa' }}>
             <NoBalance />
         </section>
     );
